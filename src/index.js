27983c587a940ddea10673e4002be64b7dd5d79f
Courtney Jackson <courtneymjackson8@gmail.com>
2024-11-15 21:57:28 -0500

    test ignore

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// is this ignored???
+// is this ignored?
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
